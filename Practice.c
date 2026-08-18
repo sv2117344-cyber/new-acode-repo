41573adf5ae44d35831de5ed3aefc6f28db1d2c8
sv2117344-cyber <sv2117344@gmail.com>
2026-08-18 10:33:24 +0530

    update Practice.c

---
+++ Practice.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+int main () {
+  int x = 10;
+  int x = 3;
+  
+  printf("%d\n", x + y); //13
+  printf("%d\n", x - y); //7
+  printf("%d\n", x * y); //3
+  printf("%d\n", x / y); //3
+  printf("%d\n", x % y); //1
+  
+  int z = 5;
+  z++;
+  printf("%d\n", ++z*++z)); //6 
+  printf"%d\n" , z);
+  --z;
+  printf"%d\n" , z); //5
+  
+  return 0;
+}
